70a32ced928bc2d730d95f8a187073b87d65366e
JasonChang1978 <jasonc.csy@gmail.com>
2025-04-25 14:23:11 +0800

    write collected data to google sheet

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ package main
 */
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -25,10 +27,33 @@ func trackHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	now := time.Now().Format("2006-01-02 15:04:05")
-	logEntry := fmt.Sprintf("ID: %s, Timestamp: %s", id, now)
-	fmt.Println(logEntry) // 輸出到標準輸出
+
+	// 準備要發送的資料
+	postData := map[string]string{
+		"id":        id,
+		"timestamp": now,
+	}
+	jsonData, err := json.Marshal(postData)
+	if err != nil {
+		http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+		return
+	}
+
+	// Google Apps Script 的網路應用程式 URL (請替換為您的實際 URL)
+	appsScriptURL := "https://script.google.com/macros/s/AKfycbxcJw0LmJtWjbEEs-UAs5ns9na_vdDsygC_E-nB3vHtbgZfkV6EOKKGqXNRu5_K3DSgnA/exec"
+
+	// 發送 HTTP POST 請求
+	resp, err := http.Post(appsScriptURL, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Println("Error sending request to Apps Script:", err)
+		http.Error(w, "Error logging data", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Data sent to Google Sheets. Status:", resp.Status)
 
 	// 顯示訊息
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<p>You are cheating! id="+id+"!中文測試</p>")
+	fmt.Fprint(w, "<p>您已完成帳號確認</p>")
 }
